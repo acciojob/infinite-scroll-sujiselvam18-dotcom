68cad94e42e9047d5fcdc26944ae5a84c996578c
Sujitha.S <sujiselvam18@gmail.com>
2026-04-22 22:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let count = 0;
+
+// Function to add items
+function addItems(num) {
+    for (let i = 0; i < num; i++) {
+        count++;
+        const li = document.createElement("li");
+        li.textContent = "List Item " + count;
+        list.appendChild(li);
+    }
+}
+
+
+addItems(10);
+
+
+list.addEventListener("scroll", () => {
+    // Check if scrolled to bottom
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+        addItems(2);
+    }
+});
